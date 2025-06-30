e9275ac3daf787967029d3a0a2b18a8ee7923af8
Letscrypto25 <letscrypto25@gmail.com>
2025-06-30 22:41:50 +0200

    Create scripts.jshereq

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Mobile menu toggle
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Add animations here
